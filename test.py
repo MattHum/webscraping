eeb29720b9849d2da09347d133967db2fc77c6b8
MattHum <m.humpeler@hotmail.com>
2023-11-11 21:30:58 +0100

    erster test 1140 funkt

--- test.py
+++ test.py
@@ -1,9 +1,20 @@
+import time
+from selenium.webdriver.common.by import By
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service as ChromeService
+driver = webdriver.Chrome()
+
+#tips
+#element = driver.find_element(By.ID, "passwd-id")
+#element = driver.find_element(By.NAME, "passwd")
+#element = driver.find_element(By.XPATH, "//input[@id='passwd-id']")
+#element = driver.find_element(By.CSS_SELECTOR, "input#passwd-id")
 
 #edikte
-# Bundeslandauswahl -> dev 'col-sm-9'
-# Suchfunktion -> 
+# Bundeslandauswahl -> dev 'col-sm-9' /select name "BL, tabindex ="19
+# PLZ -> dev 'col-sm-9' / input name "VPLZ" value tabindex="18"
+# Suchfunktion -> class="col-sm-9 col-sm-offset-3" /name "sebut" class "btn btn-primary"
 
 # hier weiter lesen: https://selenium-python.readthedocs.io/navigating.html
 
@@ -15,7 +26,14 @@ options.add_argument("--window-size=1920,1200")
 driver = webdriver.Chrome(options=options)
 driver.get("https://edikte.justiz.gv.at/edikte/ex/exedi3.nsf/suche!OpenForm&subf=vex")
 
-h1 = driver.find_element(By.CLASS_NAME, 'col-sm-9')
+h1 = driver.find_element(By.NAME, "VPLZ")
+h1.send_keys("1140")
 
-print(driver.page_source)
+# Assume the button has the ID "submit" :)
+driver.find_element(By.CLASS_NAME, "btn.btn-primary").click()
+
+time.sleep(30) # Let the user actually see something!
+#driver.quit()
+
+#print(driver.page_source)
 driver.quit()
